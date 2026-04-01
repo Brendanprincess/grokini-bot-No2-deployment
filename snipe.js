9472a36d547e57d35ec010c265dc5c380063be16
brendan <brendanmiracle101@gmail.com>
2026-04-01 13:11:35 +0100

    Change microAdjust value from 2.5 to 3
    Adjusted the micro adjustment value for logo rendering.

--- snipe.js
+++ snipe.js
@@ -606,7 +606,7 @@ async function generatePnLImage(data) {
     const logoMargin = 6;
 
     // Optional micro tweak if needed (usually 0 or 1)
-    const microAdjust = 2.5;
+    const microAdjust = 3;
 
     ctx.drawImage(
       logo,
