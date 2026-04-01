79acf5f29af9cfe583bff82df19ae2f9a5974eaf
brendan <brendanmiracle101@gmail.com>
2026-04-01 14:12:51 +0100

    Adjust microAdjust value for logo positioning
    Increased the microAdjust value for logo positioning.

--- snipe.js
+++ snipe.js
@@ -606,7 +606,7 @@ async function generatePnLImage(data) {
     const logoMargin = 6;
 
     // Optional micro tweak if needed (usually 0 or 1)
-    const microAdjust = 4;
+    const microAdjust = 6;
 
     ctx.drawImage(
       logo,
